a7ac5ba332e427df30841e707bae12dd657fca5b
shrln <shrln0@proton.me>
2024-11-17 23:01:55 +0100

    Simple implementation for JS
    Simple implementation of a code generator which translates intermediate representation bytecode to JavaScript code.

---
+++ src/app/engram.ts
@@ -0,0 +1,45 @@
+type Bytecode = { opcode: number; operand: bigint }[];
+
+const OperatorsJS = ["&", "|", "^", "+", "-", "*", "/", "<<", ">>", "%", "**"];
+
+/**
+ * Generates a JavaScript script from a bytecode.
+ * @param bytecode The bytecode used to generate the JavaScript script.
+ * It must be of type `{ opcode: number, operand: bigint }[]`.
+ * With an empty bytecode, the script will only contain `0n`.
+ * @returns The JavaScript script generated from the bytecode
+ */
+export function generateJS(bytecode: Bytecode): string {
+    const lines = new Array<Array<string>>();
+    {
+        const first = 0;
+        let current = 1;
+        const last = bytecode.length;
+
+        lines[first] = [];
+
+        for (const instruction of bytecode) {
+            const opcode = OperatorsJS[instruction.opcode];
+            const operand = instruction.operand.toString();
+            lines[current] = [" ", opcode, " ", operand, "n", ")"];
+
+            lines[first].push("(");
+            current += 1;
+        }
+
+        {
+            // This block of code is not necessary for the script to work
+            lines[first].pop(); // remove unnecessary "("
+            lines[last].pop(); // remove unnecessary ")"
+            lines[last].push(";");
+        }
+
+        // Add inital value
+        lines[first].push("0", "n");
+    }
+
+    const script = lines.flat().join("");
+    console.log(script);
+
+    return script;
+}
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,83 @@
+import { generateJS } from "./engram.ts";
+
 export default function main() {
-    console.log("Hello, world!");
+    {
+        console.log("Test n°1");
+        const bytecode = [
+            { opcode: 1, operand: 0b00000001n },
+            { opcode: 3, operand: 0b00011111n },
+            { opcode: 2, operand: 0b00011111n },
+            { opcode: 0, operand: 0b00111000n },
+            { opcode: 8, operand: 3n },
+        ];
+        const program = generateJS(bytecode);
+        // console.log(program);
+        const result = eval(program);
+        // console.log(toBinary(result, 8));
+        // console.log("     ^^^");
+    }
+    {
+        console.log("Test n°2");
+        const bytecode = [
+            { opcode: 1, operand: 0b00011n }, // input
+            { opcode: 0, operand: 0b00011n }, // input mask
+
+            { opcode: 3, operand: 0b00101n },
+            { opcode: 2, operand: 0b00100n },
+            { opcode: 3, operand: 0b00111n },
+            { opcode: 0, operand: 0b10111n },
+            { opcode: 2, operand: 0b00010n },
+            { opcode: 3, operand: 0b00101n },
+            { opcode: 2, operand: 0b00100n },
+            { opcode: 4, operand: 0b00001n },
+            { opcode: 2, operand: 0b00010n },
+            { opcode: 3, operand: 0b00111n },
+
+            { opcode: 8, operand: 3n }, // output shift
+            { opcode: 0, operand: 0b00011n }, // output mask
+        ];
+        const program = generateJS(bytecode);
+        // console.log(program);
+        const result = eval(program);
+        // console.log(toBinary(result, 5)); // memory &= 0b00011n;
+        // console.log("   ^^");
+    }
+    {
+        console.log("Test n°3");
+        const bytecode = [
+            { opcode: 1, operand: 0b00000_00001_00010_00011n }, // input
+            { opcode: 0, operand: 0b00011_00011_0001100011n }, // input mask
+
+            { opcode: 3, operand: 0b00101_00101_00101_00101n },
+            { opcode: 2, operand: 0b00100_00100_00100_00100n },
+            { opcode: 3, operand: 0b00111_00111_00111_00111n },
+            { opcode: 0, operand: 0b10111_10111_10111_10111n },
+            { opcode: 2, operand: 0b00010_00010_00010_00010n },
+            { opcode: 3, operand: 0b00101_00101_00101_00101n },
+            { opcode: 2, operand: 0b00100_00100_00100_00100n },
+            { opcode: 4, operand: 0b00001_00001_00001_00001n },
+            { opcode: 2, operand: 0b00010_00010_00010_00010n },
+            { opcode: 3, operand: 0b00111_00111_00111_00111n },
+
+            { opcode: 8, operand: 3n }, // output shift
+            { opcode: 0, operand: 0b00011_00011_00011_00011n }, // output mask
+        ];
+        const program = generateJS(bytecode);
+        // console.log(program);
+        const result = eval(program);
+        // console.log(toBinary(result, 20)); // memory &= 0b00011n;
+        // console.log("   ^^   ^^   ^^   ^^");
+    }
+    {
+        console.log("Test n°4");
+        const bytecode = [{ opcode: 0, operand: 0n }];
+        const program = generateJS(bytecode);
+        const result = eval(program);
+        // console.log(toBinary(result, 8));
+        // console.log("        ");
+    }
+}
+
+function toBinary(n: bigint, padding = 0): string {
+    return n.toString(2).replace("-", "").padStart(padding, "0");
 }
