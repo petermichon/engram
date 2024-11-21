795ea2b94a3dda9a1a5b29cad215a73b7296b641
shrln <shrln0@proton.me>
2024-11-21 19:54:42 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { generateJS } from "./engram.ts";
 
 function main() {
-    {
-        const bytecode = [
-            { opcode: 0, operand: 1 }, // [0] = [1]
-            { opcode: 7, operand: 1 }, // [0] += 1
-            { opcode: 1, operand: 1 }, // [1] = [0]
-        ];
-        const script = generateJS(bytecode, 4);
-        console.log(script);
-        console.log(eval(script).toString());
-    }
+    const bytecode = [
+        { opcode: 0, operand: 1 }, // [0] = [1]
+        { opcode: 7, operand: 1 }, // [0] += 1
+        { opcode: 1, operand: 1 }, // [1] = [0]
+    ];
+    const size = 2;
+    const script = generateJS(bytecode, size);
+
+    console.log(script);
 }
 
 main();
